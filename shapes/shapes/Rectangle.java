b4436f0a77ac4d4ba10732c38c603bbb72c00edf
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2017-08-22 20:16:31 -0500

    Primera subida
    Primer laboratorio de poob 2017-2

---
+++ shapes/shapes/Rectangle.java
@@ -0,0 +1,238 @@
+import java.awt.*;
+
+/**
+ * A rectangle that can be manipulated and that draws itself on a canvas.
+ * 
+ * @author  Paola Cuellar y Nicolás Cárdenas
+ * @version 2.0  (22 August 2017)
+ */
+
+
+ 
+public class Rectangle{
+
+    private int height;
+    private int width;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private boolean isVisible;
+    private String ColorOrigin;
+    private int perimeter;
+
+    /**
+     * Create a new rectangle at default position with default color.
+     */
+    public Rectangle(){
+        height = 30;
+        width = 40;
+        xPosition = 70;
+        yPosition = 15;
+        color = "magenta";
+        isVisible = false;
+        perimetro();
+    }
+    
+
+    /**
+     * Make this rectangle visible. If it was already visible, do nothing.
+     */
+    public void makeVisible(){
+        isVisible = true;
+        draw();
+    }
+    
+    /**
+     * Make this rectangle invisible. If it was already invisible, do nothing.
+     */
+    public void makeInvisible(){
+        erase();
+        isVisible = false;
+    }
+    
+    /**
+     * Move the rectangle a few pixels to the right.
+     */
+    public void moveRight(){
+        moveHorizontal(20);
+    }
+
+    /**
+     * Move the rectangle a few pixels to the left.
+     */
+    public void moveLeft(){
+        moveHorizontal(-20);
+    }
+
+    /**
+     * Move the rectangle a few pixels up.
+     */
+    public void moveUp(){
+        moveVertical(-20);
+    }
+
+    /**
+     * Move the rectangle a few pixels down.
+     */
+    public void moveDown(){
+        moveVertical(20);
+    }
+
+    /**
+     * Move the rectangle horizontally.
+     * @param distance the desired distance in pixels
+     */
+    public void moveHorizontal(int distance){
+        erase();
+        xPosition += distance;
+        draw();
+    }
+
+    /**
+     * Move the rectangle vertically.
+     * @param distance the desired distance in pixels
+     */
+    public void moveVertical(int distance){
+        erase();
+        yPosition += distance;
+        draw();
+    }
+
+    /**
+     * Slowly move the rectangle horizontally.
+     * @param distance the desired distance in pixels
+     */
+    public void slowMoveHorizontal(int distance){
+        int delta;
+
+        if(distance < 0) {
+            delta = -1;
+            distance = -distance;
+        } else {
+            delta = 1;
+        }
+
+        for(int i = 0; i < distance; i++){
+            xPosition += delta;
+            draw();
+        }
+    }
+
+    /**
+     * Slowly move the rectangle vertically.
+     * @param distance the desired distance in pixels
+     */
+    public void slowMoveVertical(int distance){
+        int delta;
+
+        if(distance < 0) {
+            delta = -1;
+            distance = -distance;
+        } else {
+            delta = 1;
+        }
+
+        for(int i = 0; i < distance; i++){
+            yPosition += delta;
+            draw();
+        }
+    }
+
+    /**
+     * Change the size to the new size
+     * @param newHeight the new height in pixels. newHeight must be >=0.
+     * @param newWidht the new width in pixels. newWidth must be >=0.
+     */
+    public void changeSize(int newHeight, int newWidth) {
+        erase();
+        height = newHeight;
+        width = newWidth;
+        draw();
+    }
+    
+    /**
+     * Change the color. 
+     * @param color the new color. Valid colors are "red", "yellow", "blue", "green",
+     * "magenta" and "black".
+     */
+    public void changeColor(String newColor){
+        color = newColor;
+        draw();
+    }
+
+    /*
+     * Draw the rectangle with current specifications on screen.
+     */
+
+    private void draw() {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.draw(this, color,
+                new java.awt.Rectangle(xPosition, yPosition, width, height));
+            canvas.wait(10);
+        }
+    }
+
+    /*
+     * Erase the rectangle on screen.
+     */
+    private void erase(){
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+    /*
+     * Wait for a specified number of milliseconds before finishing.
+     * This provides an easy way to specify a small delay which can be
+     * used when producing animations.
+     * @param  milliseconds  the number 
+     */
+    private void Wait(int milliseconds){
+        try{
+            Thread.sleep(milliseconds);
+        } catch (Exception e){
+            // ignoring exception at the moment
+        }
+    }
+    /**
+     *To go through the colors of the rainbow.
+     *@author  Paola Cuellar y Nicolás Cárdenas
+     *@version 1.0  (22 August 2017)
+     */
+    public void rainbow(){
+        ColorOrigin = color;
+        changeColor("red");
+        Wait(100);
+        changeColor("orange");
+        Wait(100);
+        changeColor("yellow");
+        Wait(100);
+        changeColor("green");
+        Wait(100);
+        changeColor("blue");
+        Wait(100);
+        changeColor("purple");
+        Wait(100);
+        changeColor("magenta");
+        Wait(100);
+        changeColor(ColorOrigin);
+    }
+    /**
+     *Doble the size of the rectangle.
+     *@author  Paola Cuellar y Nicolás Cárdenas
+     *@version 1.0  (22 August 2017)
+     */
+    public void twice(){
+        height*=2;
+        width*=2;
+        makeVisible();
+    }
+    /**
+     * 
+     */
+    public void perimetro(){
+        perimeter = (2*height)+(2*width);
+    }
+}
+
